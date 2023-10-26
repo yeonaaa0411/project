8728775998aecd1c5ae8bb1a06a720f114ec269f
Marvin Kristian M. Cruz <143852958+yeonaaa0411@users.noreply.github.com>
2023-10-26 09:59:54 +0800

    Update db_ba3101-Group-1.sql

--- db_ba3101-Group-1.sql
+++ db_ba3101-Group-1.sql
@@ -47,73 +47,106 @@ INSERT INTO `tbadmin` (`Username`, `Password`) VALUES
 --
 
 DROP TABLE IF EXISTS `tbclient`;
-CREATE TABLE IF NOT EXISTS `tbclient` (
-  `plate_Number` varchar(50) NOT NULL,
-  `vehicle_Type` char(50) DEFAULT NULL,
-  `first_Name` char(50) DEFAULT NULL,
-  `last_Name` char(50) DEFAULT NULL,
-  `sr_Code` varchar(50) DEFAULT NULL,
-  `Department` char(50) DEFAULT NULL,
-  `type` char(50) DEFAULT NULL,
-  PRIMARY KEY (`plate_Number`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+-- Create the 'tb_admin' table
+CREATE TABLE IF NOT EXISTS tb_admin (
+  admin_ID INT AUTO_INCREMENT,
+  admin_name VARCHAR(255) NOT NULL,
+  admin_password VARCHAR(20) NOT NULL,
+  PRIMARY KEY (admin_ID)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `tbclient`
---
+-- Create the 'tb_department' table
+CREATE TABLE IF NOT EXISTS tb_department (
+  department_id INT AUTO_INCREMENT,
+  department_name VARCHAR(255) NOT NULL,
+  PRIMARY KEY (department_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-INSERT INTO `tbclient` (`plate_Number`, `vehicle_Type`, `first_Name`, `last_Name`, `sr_Code`, `Department`, `type`) VALUES
-('VFA-175', 'Car', 'Rafael', 'Acda', '21-37452', '', 'Student'),
-('BAV-163', 'Car', 'Dioneces', 'Alimoren', '', 'CICS', 'Professor'),
-('BAK-875', 'Car', 'Francis', 'Balazon', '', 'CICS', 'Professor'),
-('BAT-347', 'Car', 'Jonnah', 'Melo', '', 'CICS', 'Professor'),
-('DAD-808', 'Car', 'Ryndel', 'Amorado', '', 'CICS', 'Professor');
+-- Create the 'tb_event' table
+CREATE TABLE IF NOT EXISTS tb_event (
+  event_id INT AUTO_INCREMENT,
+  event_title VARCHAR(255) NOT NULL,
+  event_detail TEXT NOT NULL,
+  event_date DATE NOT NULL,
+  department_id INT,
+  PRIMARY KEY (event_id),
+  FOREIGN KEY (department_id) REFERENCES tb_department (department_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
+-- Create the 'tb_students' table
+CREATE TABLE IF NOT EXISTS tb_students (
+  student_id INT AUTO_INCREMENT,
+  student_name VARCHAR(255) NOT NULL,
+  student_password VARCHAR(20) NOT NULL,
+  department_id INT,
+  PRIMARY KEY (student_id),
+  FOREIGN KEY (department_id) REFERENCES tb_department (department_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Table structure for table `tblogs`
---
+-- Create the 'tb_rso' table
+CREATE TABLE IF NOT EXISTS tb_rso (
+  rso_id INT AUTO_INCREMENT,
+  rso_name VARCHAR(255) NOT NULL,
+  rso_password VARCHAR(20) NOT NULL,
+  department_id INT,
+  PRIMARY KEY (rso_id),
+  FOREIGN KEY (department_id) REFERENCES tb_department (department_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-DROP TABLE IF EXISTS `tblogs`;
-CREATE TABLE IF NOT EXISTS `tblogs` (
-  `plate_Number` varchar(50) NOT NULL,
-  `recordDate` date DEFAULT NULL,
-  `time_In` time DEFAULT NULL,
-  `time_Out` time DEFAULT NULL,
-  KEY `plate_Number` (`plate_Number`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+-- Create the 'tb_faculty' table
+CREATE TABLE IF NOT EXISTS tb_faculty (
+  faculty_id INT AUTO_INCREMENT,
+  faculty_name VARCHAR(255) NOT NULL,
+  faculty_password VARCHAR(20) NOT NULL,
+  department_id INT,
+  PRIMARY KEY (faculty_id),
+  FOREIGN KEY (department_id) REFERENCES tb_department (department_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
+-- Insert data into 'tb_admin' table
+INSERT INTO tb_admin (admin_name, admin_password) VALUES
+  ('Admin 1', 'admin1_password'),
+  ('Admin 2', 'admin2_password');
 
---
--- Table structure for table `tbstaff`
---
+-- Insert data into 'tb_department' table
+INSERT INTO tb_department (department_name) VALUES
+  ('Department 1'),
+  ('Department 2');
 
-DROP TABLE IF EXISTS `tbstaff`;
-CREATE TABLE IF NOT EXISTS `tbstaff` (
-  `user_ID` int NOT NULL AUTO_INCREMENT,
-  `last_Name` char(50) DEFAULT NULL,
-  `first_Name` char(50) DEFAULT NULL,
-  `Gender` char(50) DEFAULT NULL,
-  `Contact` varchar(11) DEFAULT NULL,
-  `Address` char(50) DEFAULT NULL,
-  `Email` varchar(50) DEFAULT NULL,
-  `Password` char(50) DEFAULT NULL,
-  PRIMARY KEY (`user_ID`)
-) ENGINE=MyISAM AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+-- Insert data into 'tb_event' table
+INSERT INTO tb_event (event_title, event_detail, event_date, department_id) VALUES
+  ('Event 1', 'Event 1 Details', '2023-10-25', 1),
+  ('Event 2', 'Event 2 Details', '2023-11-15', 2);
 
---
--- Dumping data for table `tbstaff`
---
+-- Insert data into 'tb_students' table
+INSERT INTO tb_students (student_name, student_password, department_id) VALUES
+  ('Student 1', 'student1_password', 1),
+  ('Student 2', 'student2_password', 1);
 
-INSERT INTO `tbstaff` (`user_ID`, `last_Name`, `first_Name`, `Gender`, `Contact`, `Address`, `Email`, `Password`) VALUES
-(16, 'Castillo', 'Dianne', 'Female', '09063524024', 'San Lucas Lipa City', 'dayan@gmail.com,', 'dayan'),
-(17, 'Alday', 'Keon', 'Male', '09012356792', 'Brgy. Dos Lipa City', 'keon@gmail.com', 'keon'),
-(18, 'Caniete', 'Cristel', 'Female', '09446531859', 'San Juan Batangas', 'cristel@gmail.com', 'cristel'),
-(19, 'Cuya', 'Lester', 'Male', '09877361128', 'San Juan Batangas', 'lester@gmail.com', 'lester'),
-(20, 'Acda', 'Rafael', 'Male', '09053127397', 'Cumba Lipa City', 'rafael@gmail.com', 'rafael');
-COMMIT;
+-- Insert data into 'tb_rso' table
+INSERT INTO tb_rso (rso_name, rso_password, department_id) VALUES
+  ('RSO 1', 'rso1_password', 2),
+  ('RSO 2', 'rso2_password', 2);
+
+-- Insert data into 'tb_faculty' table
+INSERT INTO tb_faculty (faculty_name, faculty_password, department_id) VALUES
+  ('Faculty 1', 'faculty1_password', 1),
+  ('Faculty 2', 'faculty2_password', 2);
+
+
+-- Create the 'tb_attendees' table with separate columns for each role
+CREATE TABLE IF NOT EXISTS tb_attendees (
+  attendee_id INT AUTO_INCREMENT,
+  event_id INT NOT NULL,
+  student_id INT,
+  faculty_id INT,
+  rso_id INT,
+  PRIMARY KEY (attendee_id),
+  FOREIGN KEY (event_id) REFERENCES tb_event (event_id),
+  FOREIGN KEY (student_id) REFERENCES tb_students (student_id) ON DELETE CASCADE,
+  FOREIGN KEY (faculty_id) REFERENCES tb_faculty (faculty_id) ON DELETE CASCADE,
+  FOREIGN KEY (rso_id) REFERENCES tb_rso (rso_id) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
