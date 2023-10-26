5d358f7cc4125d02db1eef2edded77727588d0b1
Marvin Kristian M. Cruz <143852958+yeonaaa0411@users.noreply.github.com>
2023-10-26 10:56:41 +0800

    Update db_ba3101.sql

--- db_ba3101.sql
+++ db_ba3101.sql
@@ -27,24 +27,6 @@ SET time_zone = "+00:00";
 -- Table structure for table `tbadmin`
 --
 
-DROP TABLE IF EXISTS `tbadmin`;
-CREATE TABLE IF NOT EXISTS `tbadmin` (
-  `Username` char(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `Password` char(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `tbadmin`
---
-
-INSERT INTO `tbadmin` (`Username`, `Password`) VALUES
-('admin', '123');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tbclient`
---
 
 CREATE TABLE Students (
     SR_Code INT PRIMARY KEY,
